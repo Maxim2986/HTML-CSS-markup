9a9666907c788fcd2f90995fe43e3ec4de599530
Maxim2986 <maxim.ostrovskiy86@gmail.com>
2019-09-26 13:06:26 +0300

    add about section and benefits section

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
 // Slider
-// var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 1,
-//     loop: true,
-//     effect: "fade",
-//     speed: 1000,
-//     loopFillGroupWithBlank: true,
-//     autoplay: {
-//         delay: 5000
-//     },
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true
-//     },
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev'
-//     }
-// });
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    // effect: "fade",
+    speed: 1000,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 5000
+    },
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
 
 // Menu
 $(document).ready(function () {
